85a8bbe2ae5d65dd8718f3fe28f4c14d13d3caab
Gabriel Lamego <gglamego@gmail.com>
2025-01-20 19:17:45 -0300

    Update transaction.cpp

--- src/transaction.cpp
+++ src/transaction.cpp
@@ -1,9 +1,33 @@
 #include "transaction.hpp"
 #include <sstream>
 #include <iomanip>
+#include <iostream>
 
-Transacao::Transacao(const Acao& acao, int quant, float valor, TipoTransacao t)
-    : acao(acao), quantidade(quant), preco(valor), tipo(t), momento(std::time(nullptr)) {}
+Transacao::Transacao(string user, Acao stock, int quant, float valor, TipoTransacao t):
+ usuario(user), acao(stock), quantidade(quant), preco(valor), tipo(t), momento(std::time(nullptr)) {}
+
+time_t stringParaTimeT(const std::string& dataHora) {
+    struct tm tm = {0};
+    
+    std::stringstream ss(dataHora);
+    ss >> std::get_time(&tm, "%d/%m/%Y-%H:%M:%S");
+    
+    if (ss.fail()) {
+        std::cout << "Falha ao converter data/hora." << std::endl;
+        return -1;
+    }
+
+    return mktime(&tm);
+}
+
+Transacao::Transacao(string tempo, string usuario, string tipo, Acao stock, int quant){
+    this->momento = stringParaTimeT(tempo);
+    this->usuario = usuario;
+    this->acao = stock;
+    this->quantidade = quant;
+    this->tipo = (tipo == "Compra") ? TipoTransacao::COMPRA : TipoTransacao::VENDA;
+    this->preco = acao.getPreco()*quant;
+}
 
 Acao Transacao::getAcao() const {
     return acao;
@@ -26,11 +50,14 @@ std::time_t Transacao::getMomento() const {
 }
 
 std::string Transacao::toString() const {
+    std::tm* local_time = std::localtime(&momento);
+
     std::ostringstream oss;
-    oss << "Tipo: " << (tipo == TipoTransacao::BUY ? "Compra" : "Venda") << "\n"
-        << "Ação: " << acao.getCodigo() << "\n"
-        << "Quantidade: " << quantidade << "\n"
-        << "Preço: " << std::fixed << std::setprecision(2) << preco << "\n"
-        << "Data/Hora: " << std::asctime(std::localtime(&momento));
+    oss << std::put_time(local_time, "%d/%m/%Y-%H:%M:%S") << "|"
+    << usuario << "|" 
+    << (tipo == TipoTransacao::COMPRA ? "Compra" : "Venda") << "|"
+    << acao.getCodigo() << "|"
+    << quantidade << "|"
+    << std::fixed << std::setprecision(2) << preco;
     return oss.str();
 }
